f5ace5ab6efad867cdf4d4d9928823b9988ebcb5
Cristian Montes <cmontesmujica@gmail.com>
2021-09-11 14:53:08 -0700

    longest common prefix-done

---
+++ index.js
@@ -0,0 +1,21 @@
+//#14 LONGEST COMMON PREFIX
+var longestCommonPrefix = function(strs) {
+    if(!strs.length)return '';
+    for(let i=0; i < strs[0].length; i++){
+      for(let j=1; j<strs.length; j++){
+        if(strs[0][i] !== strs[j][i]){
+          return strs[0].slice(0, i);
+        }
+      }
+    }
+
+    return strs[0];
+}
+
+//#15 3Sum GIVEN AN INTEGER ARRAY NUMS, RETURN ALL THE TRIPLETS
+let nums = [-1,0,1,2,-1,-4]
+
+var threeSum = function(nums){
+
+}
+
