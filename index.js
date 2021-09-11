1b52d0a325c35c6c69a2cadee6e71fbe69a5dfdd
Cristian Montes <cmontesmujica@gmail.com>
2021-09-11 15:49:15 -0700

    3Sum - done

--- index.js
+++ index.js
@@ -13,9 +13,34 @@ var longestCommonPrefix = function(strs) {
 }
 
 //#15 3Sum GIVEN AN INTEGER ARRAY NUMS, RETURN ALL THE TRIPLETS
-let nums = [-1,0,1,2,-1,-4]
+let numeros= [-1,0,1,2,-1,-4]
 
 var threeSum = function(nums){
+  let sorted = nums.sort((a,b)=> a-b);
+  let triplets = [];
 
-}
+  for(let i=0; i<nums.length-2; i++){
+      if(nums[i] != nums[i-1]){
+          let left = i+1;
+          let right = nums.length-1
+          while (left < right){
+              const currentSum = nums[i] + nums[left] + nums[right];
+              if(currentSum === 0){
+                  triplets.push([nums[i], nums[left], nums[right]]);
+                  while(nums[left] == nums[left + 1]) left ++
+                  while(nums[right] == nums[right -1]) right --;
+                  left ++;
+                  right --;
+              } else if (currentSum < 0){
+                  left ++;
+              } else if(currentSum > 0){
+                  right  --;
+              }
+          }
+      }
+  }
+  return triplets;
+};
+
+threeSum(numeros);
 
