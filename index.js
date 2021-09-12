c8f13253b1d9c2b8c92f14cd9504c807fbe417f8
Cristian Montes <cmontesmujica@gmail.com>
2021-09-12 11:44:43 -0700

    merged two sorted list-done

--- index.js
+++ index.js
@@ -67,5 +67,23 @@ let isValid = function(s){
     return stack.length ? false : true;
 };
 
-isValid(noClosy);
+// isValid(noClosy);
+
+// 21 MERGE TWO SORTED LIST
+l1 = [1,2,4], l2 = [1,3,4];
+
+var mergeTwoLists = function(l1, l2) {
+   if(!l1) return l2;
+   if(!l2) return l1;
+
+   if(l1.val < l2.val){
+       l1.next = mergeTwoLists(l1.next, l2);
+       console.log(l1);
+       return l1;
+   }
+   l2.next = mergeTwoLists(l2.next, l1);
+   return l2
+};
+
+mergeTwoLists(l1,l2)
 
