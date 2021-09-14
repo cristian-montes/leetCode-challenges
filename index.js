891b7dd57d72f8f47fa4ca396ffd35fecfb223e8
Cristian Montes <cmontesmujica@gmail.com>
2021-09-13 18:58:50 -0700

    maximum subarray-done

--- index.js
+++ index.js
@@ -144,7 +144,23 @@ var searchInsert = function(nums, target) {
     }
     return nums.length
 };
-
 // searchInsert(nums, target)
 
-// 
+// 53. MAXIMUM SUBARRAY
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+var maxSubArray = function(nums) {
+    if(nums.length === 0) return 0;
+    
+    let max = nums[0]
+    for(let i = 0; i < nums.length; i++){
+        console.log(nums[i-1])
+
+      if(nums[i-1] > 0){
+          nums[i] += nums[i-1];
+      }
+      max = Math.max(nums[i], max)
+    }
+    return max;
+};
+
+maxSubArray(nums)
