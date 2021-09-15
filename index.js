2f60769c6711b0146d55890ea72deb85e2c7691d
Cristian Montes <cmontesmujica@gmail.com>
2021-09-15 08:43:40 -0700

    added clarification comments to mySqrt function

--- index.js
+++ index.js
@@ -248,15 +248,18 @@ var mySqrt = function(x) {
     let left = 1;
     let right = x;
 
-    while(left < right){
-        const mid = Math.floor((left + right)/2);
-        if(mid * mid === x ) return mid;
-        else if(mid * mid > x) right = mid
-        else left = mid+1;
-    }
-  console.log(right);
-  return left-1;
-};
+// Use binary search to find the square root or the whole number closest to the square root
+    while(left < right) {
+// Find the mid point between left and right
+    const mid = Math.floor((left + right) / 2)
+// Return the mid point if this is the square root
+    if(mid*mid === x) return mid
+// If mid squared is greater than x then the answer must be on the left half of mid
+    else if(mid*mid >x) right = mid
+// If mid squred is less than x then the answer must be on the right half of mid
+    else left = mid+1
+    };
+}
 
 const x = 4;
 mySqrt(x);
