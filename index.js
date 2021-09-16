92fb338978ef5b6ab594ef68925f1f99e6fff69c
Cristian Montes <cmontesmujica@gmail.com>
2021-09-16 08:39:50 -0700

    remove dup. from a sorted linked list

--- index.js
+++ index.js
@@ -279,5 +279,20 @@ var climbStairs = function(n) {
 };
 
 
-const n = 3;
-climbStairs(n)
+// const n = 3;
+// climbStairs(n)
+
+//83. REMOVE DUPLICATES FROM SORTED LIST
+var deleteDuplicates = function(head) {
+    let  current = head;
+
+    while(current !==null && current.next !== null){
+        if(current.val === current.next.val){
+            curent.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return head
+};
+
