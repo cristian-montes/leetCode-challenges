24b52df87dd504489864a8a4f8bcbc3052e71022
Cristian Montes <cmontesmujica@gmail.com>
2021-09-17 08:06:37 -0700

    88. Merge Sorted Array

--- index.js
+++ index.js
@@ -296,3 +296,18 @@ var deleteDuplicates = function(head) {
     return head
 };
 
+// 88. Merge Sorted Array
+var merge = function(nums1, m, nums2, n) {
+    while(n){
+        if(nums1[m-1]> nums2[n-1]){
+            nums1[m+n-1] = nums1[--m];
+        } else {
+            nums1[m+n-1] = nums2[--n];
+        }
+    }
+   
+    return nums1;
+};
+
+nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+merge(nums1, m, nums2, n )
