1f0e21609487187bbf3ad939f8116a55ff76520f
Cristian Montes <cmontesmujica@gmail.com>
2021-09-20 08:51:19 -0700

    Guess Number Higher or Lower - done

--- index.js
+++ index.js
@@ -428,6 +428,19 @@ var findTheDifference = function(s, t) {
        }
 };
 
-s = "abcd", t = "abcde"
+// s = "abcd", t = "abcde"
+// findTheDifference(s, t);
 
-findTheDifference(s, t);
+//374 Guess Number Higher or Lower
+var guessNumber = function(n) {
+   let left =1, right = n;
+
+    while(left < right){
+        const mid = left+Math.floor((right-left)/2);
+        const current = guess(mid);
+        if(current === 0 ) return mid;
+        if(current === -1) right = mid;
+        else left = mid+1;
+    }
+    return left
+};
