69e1c6af0dbbd8f00878b70fe39a69cbb434da6d
Cristian Montes <cmontesmujica@gmail.com>
2021-10-03 11:05:49 -0700

    majorityElement - done

--- index.js
+++ index.js
@@ -928,3 +928,24 @@ MinStack.prototype.getMin = function() {
     
 };
 // *********************************************************************************
+
+//169. MAJORITY ELEMENT
+//EXPLANATION --
+var majorityElement = function(nums) {
+    let maj = 0;
+    let count = 1;
+    for(let i = 1; i < nums.length; i++){
+       if(nums[i] ===  nums[maj]){
+           count ++
+       } else {
+           count --;
+       }
+       if(count ===0){
+           maj = i;
+           count = 1;
+       }
+    }
+    return nums[maj];
+};
+nums = [3,2,3]
+majorityElement(nums)
