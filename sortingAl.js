7de928218b2ff44eab164026c37e73f902f9a573
Cristian Montes <cmontesmujica@gmail.com>
2021-10-15 09:15:54 -0700

    working on quick sort algorithm

--- sortingAl.js
+++ sortingAl.js
@@ -79,7 +79,6 @@ function insertionSorty2(array){
 }
 
 //---------------------------------------------------------------------------------------------------------//
-/
 const numbers2= [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function mergeSorty(array){
@@ -112,6 +111,50 @@ function mergeSort(left, right){
 
     return [...sortedArr, ...left, ...right];
 }
+// const leftRight = mergeSorty(numbers2);
+// console.log(leftRight);
 
-const leftRight = mergeSorty(numbers2);
-console.log(leftRight);
+//---------------------------------------------------------------------------------------------------------//
+
+const numbers3= [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function quickSort(array, left, right){
+    const len = array.length; 
+    let pivot;
+    let partitionIndex;
+  
+    if(left < right) {
+      pivot = right;
+      partitionIndex = partition(array, pivot, left, right);
+      
+      //sort left and right
+      quickSort(array, left, partitionIndex - 1);
+      quickSort(array, partitionIndex + 1, right);
+    }
+    return array;
+  }
+     
+  function partition(array, pivot, left, right){
+    let pivotValue = array[pivot];
+    let partitionIndex = left;
+  
+    for(let i = left; i < right; i++) {
+      if(array[i] < pivotValue){
+        swap(array, i, partitionIndex);
+        partitionIndex++;
+      }
+    }
+    swap(array, right, partitionIndex);
+    return partitionIndex;
+  }
+  
+  function swap(array, firstIndex, secondIndex){
+      var temp = array[firstIndex];
+      array[firstIndex] = array[secondIndex];
+      array[secondIndex] = temp;
+  }
+
+
+//Select first and last index as 2nd and 3rd parameters
+quickSort(numbers3, 0, numbers3.length - 1);
+console.log(numbers3);
