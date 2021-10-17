368ac9859935a35cb1ceaa28c60fbe4824bc7144
Cristian Montes <cmontesmujica@gmail.com>
2021-10-16 19:46:57 -0700

    amex word pattern done

--- amex.js
+++ amex.js
@@ -1,11 +1,43 @@
 function wordPattern(pattern, s){
-   // check if s valid
-   //
-  // iterate throught the given string pattern
+// if s and pattern do not have the same length return false. 
+const word = s.split(' ');
+
+//create a map for patter.
+const mapWord = new Map();
+
+//create a map for word.
+const mapPattern = new Map();
+
+
+// iterate through pattern (for loop)
+    for(let iN = 0; iN < pattern.length; iN++){
+
+        //grab values from patter
+        const patternVal = pattern[iN];
+
+        //grab values from word
+        const wordVal = word[iN];
+
+        // check if  mapPatter have and wordPattern have the same values OR  mapPattern had && mapPattern get are not equal to word OR wordPattern has && wordPattern get is not equal to pattern --> return false.
+        if((mapPattern.has(patternVal) !== mapWord.has(wordVal)) || (mapPattern.has(patternVal) && mapPattern.get(patternVal) !== wordVal) || (mapWord.has(wordVal) && mapWord.get(wordVal) !== patternVal)){
+           
+            console.log(false);
+            return false;
+        }
+         // set mapPatter to patterkeys =  word values
+         mapPattern.set(patternVal, wordVal);
+
+         // set mapWords to wordkeys =  patter values. 
+         mapWord.set(wordVal, patternVal);
+
+    }
+    console.log(true);
+    return true
+
 
 }
 
-pattern = "abba", s = "dog cat cat dog";
+pattern = "abba", s = "dog cat cat dog"
 
 wordPattern(pattern, s);
 
