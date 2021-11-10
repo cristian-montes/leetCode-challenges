d4d287ca789382d8b5884a9f89993dd92005dcaf
Cristian Montes <cmontesmujica@gmail.com>
2021-11-10 10:09:25 -0700

    working on 26. linked list problem

--- redos.js
+++ redos.js
@@ -110,7 +110,21 @@ let stack = []
         console.log('here')
         return stack.length ? false : true;
 };
+// s = "{[]}"
+// isValid(s);
 
-s = "{[]}"
+var mergeTwoLists = function(l1, l2) {
+    if(!l1) return l2;
+    if(!l2) return l1;
 
-isValid(s);
+    if(l1.val < l2.val){
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }
+
+    l2.next = mergeTwoLists(l2.next, l1);
+    return l2;
+};
+
+l1 = [1,2,4], l2 = [1,3,4];
+mergeTwoLists(l1, l2);
