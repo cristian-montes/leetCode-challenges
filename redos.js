42b6f26a4de0248c31e8ba58992d04b515e7cc82
Cristian Montes <cmontesmujica@gmail.com>
2021-11-21 22:38:06 -0700

    added explanation to the problem

--- redos.js
+++ redos.js
@@ -201,7 +201,7 @@ var maxSubArray = function(nums) {
 
     for(let i =0; i < nums.length; i++){
         if(nums[i-1] > 0){
-            nums[i] += nums[i-1];
+            nums[i] += nums[i-1];  // this sets the existing index equal to the difference of this addition.
         }
         holder = Math.max(nums[i], holder)
     }
