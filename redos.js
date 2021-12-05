7e8b499f57ca68700fa523240ae084a7176b2f05
Cristian Montes <cmontesmujica@gmail.com>
2021-12-05 14:20:32 -0700

    merged to sorted arrays but do not return sorted array

--- redos.js
+++ redos.js
@@ -312,6 +312,30 @@ var deleteDuplicates = function(head) {
             current = current.next;
         }
     }
-
     return head;
 };
+
+// 88 MERGE SORTED ARRAY
+var merge = function(nums1, m, nums2, n) {
+    m --;
+    n --;
+
+    let i = nums1.length -1;
+    
+    while (i >=0){
+        if(n > -1 && m > -1 && nums1[m] >= nums2[n]){
+            nums1[i] = nums1[m];
+            nums1[m] = nums2[n];
+            m--;
+        }
+
+        else if (n > -1){
+            nums1[i] = nums2[n--];
+        }
+        i--
+    }
+    
+};
+
+nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+merge(nums1, m, nums2, n)
