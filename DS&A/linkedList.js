a5eb88e9f533ec6b15ead86d03976b42d6b2fbfa
Cristian Montes <cmontesmujica@gmail.com>
2022-02-08 17:24:23 -0700

    first LinkedList build in Typescript

---
+++ DS&A/linkedList.js
@@ -0,0 +1,16 @@
+//Javascript does not come with linkedlist built in.
+//https://visualgo.net/en/list --> visual representation of how linked list work
+// traversing through a Linked List is slower than iterating through an array eventhough they both have O(n) time complexity, but this is because in an array data is organized right next eachother in memory, and in a Linked List the data is all scartted around in memory.
+var LinkedList = /** @class */ (function () {
+    function LinkedList(value) {
+        this.head = {
+            value: value,
+            next: null
+        };
+        this.tail = this.head; // this is becuase one only have one item the head is also the tail.
+        this.length = 1; // 1 is assigned becuase it is only one when created.
+    }
+    return LinkedList;
+}());
+var myLinkedList = new LinkedList(10);
+console.log(myLinkedList);
---
+++ DS&A/linkedList.ts
@@ -0,0 +1,53 @@
+//Javascript does not come with linkedlist built in.
+//https://visualgo.net/en/list --> visual representation of how linked list work
+// traversing through a Linked List is slower than iterating through an array eventhough they both have O(n) time complexity, but this is because in an array data is organized right next eachother in memory, and in a Linked List the data is all scartted around in memory.
+
+//TIME COMPLEXITY OF METHODS
+// prepend O(1) -> this is when you add a node before the head.
+// append  O(1) -> this when you add a node after the tail/
+// lookup  O(N) -> transverse => when look for an specific node in memory.
+// Insert O(n) -> this when you transverse in memory to look for the node where the new is going to go... either before or after.
+// Delete O(n) -> need to transverse in memory to look for the node to be deleted.
+
+
+// EX 1    10-->5-->16-->null
+
+// this is th structure we are going to try to replacate  dynamically using A Class.
+// ****************************************************************************************************************************************************************************
+// let myLinkedList = {
+//     head:{
+//         value: 10,
+//         next: {
+//             value:5,
+//             next:{
+//                 value: 16,
+//                 next: null  // the reason this one is pointing  to null is because it is the end of LL aka. tail.
+//             }
+//         }
+//     }
+// }
+// ****************************************************************************************************************************************************************************
+
+interface nodeHead {
+    value: number;
+    next: nodeHead | null;
+}
+
+class LinkedList {
+    head: nodeHead;
+    tail: nodeHead;
+    length: number;
+
+    constructor(value){
+        this.head = {
+            value: value,
+            next: null
+        }
+
+        this.tail = this.head; // this is becuase one only have one item the head is also the tail.
+        this.length = 1;       // 1 is assigned becuase it is only one when created.
+    }
+}
+
+const myLinkedList = new LinkedList(10);
+console.log(myLinkedList);
