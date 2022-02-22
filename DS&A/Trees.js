1e09bdcdba5df4b6e49bfba119ae001141206605
Cristian Montes <cmontesmujica@gmail.com>
2022-02-21 23:21:07 -0700

    added trees.js file

--- DS&A/Trees.js
+++ DS&A/Trees.js
@@ -6,15 +6,11 @@
 // DELETE O(log N)
 // O(log N) it is just looking throught a phone book... you dont need to look through every single page, instead you go the that "section" and began looking there.you 
 // OR  devide and conquer.
-
-
 // *** HEIGH OF TREE ***//
 // # of node = 2^h - 1;
 // log nodes = height;
 // log 100 = 2;
 // 10^2 = 100;
-
-
 // FULL BINARY TREES =  there are no gaps in the tree. Either the nodes havr 0 or 2 children, but never one child. 
 //                   O
 //                  / \
@@ -23,19 +19,14 @@
 //               O   O 
 //                  / \
 //                 O   O 
-
-
 // PERFECT BINARY TREES = All node are full and there is not nodes that only have one child and the bottom nodes(leaves) are full and have 0.
 // The amount of total nodes doubles as we move down the three.
 // The sum of all of the node in the bottom leve is equal to all of the other nodes plus one or bottom =  allOtheLevels + 1.
-
 //                     O
 //                 /      \
 //                O        O 
 //               / \      / \
 //              O   O    O   O 
-
-
 // *** BINARY SEARCH TREE ***//
 //They are great for comparing things, preserves relationships
 // values to the RIGHT of the tree must be greater than the current node
@@ -44,88 +35,72 @@
 //Ordered
 //Flexible Size
 // No O(1) operations.
-
 //######### CREATE YOUR OWN BINARY SEARCH TREE ##########//
-class treeNode {
-    left: null | treeNode;
-    right: null | treeNode;
-    value: string | number
-
-    constructor(value: string | number){
+var treeNode = /** @class */ (function () {
+    function treeNode(value) {
         this.left = null;
         this.right = null;
-        this.value = value
+        this.value = value;
     }
-}
-
-class BinarySearchTree {
-    root: null |treeNode
-
-    constructor(){
+    return treeNode;
+}());
+var BinarySearchTree = /** @class */ (function () {
+    function BinarySearchTree() {
         this.root = null;
     }
-
-    insert(value: string | number){
-        const newNode =  new treeNode(value);
-
-        if(this.root === null){
+    BinarySearchTree.prototype.insert = function (value) {
+        var newNode = new treeNode(value);
+        if (this.root === null) {
             this.root = newNode;
-        } else {
-            let currentNode = this.root;
-
-            while(true){
-                if(value < currentNode.value){
+        }
+        else {
+            var currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
                     //LEFT
-                    if(!currentNode.left){
+                    if (!currentNode.left) {
                         currentNode.left = newNode;
                         return this;
                     }
                     currentNode = currentNode.left; // sets the current node to the new left to keep looping until we find a node w/o a left.
-
-                } else {
+                }
+                else {
                     //RIGHT
-                    if(!currentNode.right){
+                    if (!currentNode.right) {
                         currentNode.right = newNode;
                         return this;
                     }
                     currentNode = currentNode.right; // sets the current node to the new right to keep looping until we find a node w/o a right.
-
                 }
             }
         }
-    }
-
-    lookup(value: string | number){
-        if(!this.root){
+    };
+    BinarySearchTree.prototype.lookup = function (value) {
+        if (!this.root) {
             return false;
         }
-
-        let currentNode = this.root;
-        while(currentNode){
-            if(value < currentNode.value){
-                currentNode =  currentNode.left!
-            } else if ( value > currentNode.value){
-                currentNode = currentNode.right!
-            } else if (value ===  currentNode.value){
+        var currentNode = this.root;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            }
+            else if (value > currentNode.value) {
+                currentNode = currentNode.right;
+            }
+            else if (value === currentNode.value) {
                 return currentNode;
             }
         }
-
         return false;
-    }
-
-    //REMOVE
-
-}
-
-
-const tree =  new BinarySearchTree()
+    };
+    return BinarySearchTree;
+}());
+var tree = new BinarySearchTree();
 tree.insert(9);
 // JSON.stringify(traverse(tree.root))
 //            9
 //       4         20
 //   1      6    15   170 
-
 // function transverse(node: any){
 //     const tree = { value: node.value};
 //     tree.left =  node.left ==- null ? null : transverse(node.left);
