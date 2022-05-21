377040b3b6dbe6c0f50e456b0499c58fb0a1aa65
Cristian Montes <cmontesmujica@gmail.com>
2022-05-20 19:34:54 -0600

    added and solved Equivalent Keypress code challenge with an optimal run time of O(n) or linear time complexity

--- DS&A/SPEAK_.ts
+++ DS&A/SPEAK_.ts
@@ -51,3 +51,44 @@ function twoSum(nums: number[], target: number): number[] {
     return theMatch;
  
 };
+
+
+// EQUIVALENT KEYPRESSES
+
+function EquivalentKeypresses(strArr: string[]): string { 
+
+    // code goes here  
+  
+    let strArr1: string [] = strArr[0].split(',');
+    let strArr2: string [] =  strArr[1].split(',');
+    
+    let cleanArr1:string [] = [];
+    let cleanArr2: string [] = [];
+    for(let i = 0; i < strArr1.length; i++){
+      
+      if(strArr1[i+1] === '-B' || strArr1[i] === '-B'){
+        continue;
+      }
+  
+      cleanArr1.push(strArr1[i])
+  
+    }
+  
+    for(let i = 0; i < strArr2.length; i++){
+      
+      if(strArr2[i+1] === '-B' || strArr2[i] === '-B'){
+        continue;
+      }
+  
+      cleanArr2.push(strArr2[i])
+  
+    }
+    
+    if (cleanArr1.join() === cleanArr2.join()){
+        return 'true'
+    }
+  
+    return 'false'
+  
+  }
+     
